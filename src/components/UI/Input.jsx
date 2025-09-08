e5b832d6671fe651fa00d716d26268812b975005
soongu <naong526@naver.com>
2025-09-08 09:27:14 +0900

    가변 props 전달

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './Input.module.scss';
 
-const Input = ({ label }) => {
+// 재사용 가능한 컴포넌트 만들고 싶음.
+// 가변 props 처리방법
+const Input = ({ label, inputAttr }) => {
   return (
     <div className={styles.input}>
       <label>{label}</label>
-      <input />
+      <input {...inputAttr} />
     </div>
   );
 };
