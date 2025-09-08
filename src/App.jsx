ef55d14b0d08104b91a61b9f75da373383d97d7b
soongu <naong526@naver.com>
2025-09-08 09:29:50 +0900

    카트 모달 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Layout/Header";
 import './App.css';
 import Meals from "./components/Meals/Meals";
+import Cart from './components/Cart/Cart.jsx';
 
 const App = () => {
   return <>
+    <Cart />
     <Header />
     <div id="main">
       <Meals />
