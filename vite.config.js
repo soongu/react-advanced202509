891abaaf8b9658164c4849333869894551fea78a
Soon9 (SoonGu Hong) <naong526@naver.com>
2025-09-25 11:03:18 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+      port: 80,
+      host: '0.0.0.0'
+    },
+  },
 })
